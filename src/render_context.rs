a1c3da3648a9dbd56b5d12828148441ff9d76bd9
Nick Fitzgerald <fitzgen@gmail.com>
2019-03-28 16:00:21 -0700

    Re-introduce `Cached<R>`
    Now, instead of reusing the same `Bump` arena inside the `Cached<R>`, the vdom
    maintains a set of `Bump` arenas and cached `Node`s that are the results of
    rendering `Cached<R>`s. The entries in the set are immutable after insertion,
    and invalidating a `Cached<R>` causes it to request a new `Bump` arena from the
    vdom instead of resetting the old `Bump` that it allocated into last.
    
    But how to clean up this set of cached immutable `Bump` arenas? We have a tiny,
    specialized garbage collector for just these arenas. Because they are immutable,
    their outgoing edges never change, so we compute and store the set of transitive
    edges when inserting new arenas into the set. This means that the "mark" phase
    of our collector is super quick and easy, since it doesn't need to recursively
    mark things or maintain a mark stack. We garbage collect the arenas after each
    render.
    
    Additionally, there is a new `NodeKind::Cached` variant. This contains the id of
    its results in the set of cached nodes and their `Bump` arenas. When diffing, if
    we see that the old and new virtual dom point to the same cached subtree, then
    we avoid diffing them. This gives us really nice speed ups, and makes the new
    `Cached<R>` even better than the old version, even ignoring the bugs that the
    old version had.
    
    Fixes #31.

--- src/render_context.rs
+++ src/render_context.rs
@@ -1,11 +1,15 @@
+use crate::{
+    cached_set::{CacheId, CachedSet},
+    Node,
+};
 use bumpalo::Bump;
+use std::fmt;
 
 /// Common context available to all `Render` implementations.
 ///
 /// Notably, the `RenderContext` gives access to the bump arena that the virtual
 /// DOM should be allocated within. This is available via the `bump` field.
-#[derive(Debug)]
-pub struct RenderContext<'bump> {
+pub struct RenderContext<'a> {
     /// The underlying bump arena that virtual DOMs are rendered into.
     ///
     /// ## Example
@@ -18,35 +22,55 @@ pub struct RenderContext<'bump> {
     ///     cx.bump.alloc(42)
     /// }
     /// ```
-    pub bump: &'bump Bump,
+    pub bump: &'a Bump,
 
-    // Prevent exhaustive matching on the rendering context, so we can add more
-    // members in a semver-compatible way.
+    pub(crate) cached_set: &'a crate::RefCell<CachedSet>,
+
+    // Prevent exhaustive matching on the rendering context, so we can always
+    // add more members in a semver-compatible way.
     _non_exhaustive: (),
 }
 
-impl<'bump> RenderContext<'bump> {
-    // Don't use this! For internal use only! Subject to change in breaking ways
-    // without a breaking version bump!
-    #[doc(hidden)]
-    pub fn new(bump: &'bump Bump) -> Self {
-        RenderContext {
-            bump,
-            _non_exhaustive: (),
-        }
+impl fmt::Debug for RenderContext<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("RenderContext")
+            .field("bump", &self.bump)
+            .finish()
     }
 }
 
-impl<'a, 'bump> From<&'a RenderContext<'bump>> for &'bump Bump {
+impl<'a> RenderContext<'a> {
+    pub_unstable_internal! {
+        pub(crate) fn new(bump: &'a Bump, cached_set: &'a crate::RefCell<CachedSet>) -> Self {
+            RenderContext {
+                bump,
+                cached_set,
+                _non_exhaustive: (),
+            }
+        }
+    }
+
+    pub(crate) fn cache<F>(&self, f: F) -> CacheId
+    where
+        F: for<'b> FnOnce(&mut RenderContext<'b>) -> Node<'b>,
+    {
+        CachedSet::insert(self.cached_set, |bump, cached_set| {
+            let mut nested_cx = RenderContext::new(bump, cached_set);
+            f(&mut nested_cx)
+        })
+    }
+}
+
+impl<'a, 'b> From<&'b RenderContext<'a>> for &'a Bump {
     #[inline]
-    fn from(cx: &'a RenderContext<'bump>) -> &'bump Bump {
+    fn from(cx: &'b RenderContext<'a>) -> &'a Bump {
         cx.bump
     }
 }
 
-impl<'a, 'b, 'bump> From<&'a &'b mut RenderContext<'bump>> for &'bump Bump {
+impl<'a, 'b, 'c> From<&'c &'b mut RenderContext<'a>> for &'a Bump {
     #[inline]
-    fn from(cx: &'a &'b mut RenderContext<'bump>) -> &'bump Bump {
+    fn from(cx: &'c &'b mut RenderContext<'a>) -> &'a Bump {
         cx.bump
     }
 }
