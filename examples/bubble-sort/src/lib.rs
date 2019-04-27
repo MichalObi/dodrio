d7504f69a755c913c0ae6cbcae0fc1e123fc6633
Michal <ndwz123@gmail.com>
2019-04-27 21:43:29 +0200

    Display on screen list of vec numbers

--- examples/bubble-sort/src/lib.rs
+++ examples/bubble-sort/src/lib.rs
@@ -18,8 +18,18 @@ impl SortArray {
 /// The rendering implementation for our SortArray
 impl Render for SortArray {
     fn render<'a>(&self, cx: &mut RenderContext<'a>) -> Node<'a> {
-        use dodrio::builder::*;
-        div(&cx).finish()
+        use dodrio::builder::{li, text, ul};
+
+        let mut sort_items = bumpalo::collections::Vec::with_capacity_in(10, cx.bump);
+
+        for number in &self.state {
+            let number_as_string = bumpalo::format!(in cx.bump, "{}", number).into_bump_str();
+            sort_items.push(li(&cx).children([text(number_as_string)]).finish())
+        }
+
+        // info!("sort_items: {:#?}", sort_items);
+
+        ul(&cx).children(sort_items).finish()
     }
 }
 
@@ -36,11 +46,9 @@ pub fn run() {
     let vec: Vec<u32> = vec![1, 9, 3, 2, 4, 6, 7, 5, 0, 8];
     let sort_array = SortArray::new(vec);
 
-    info!("sort_array {:#?}", sort_array);
-
     let vdom = Vdom::new(body.as_ref(), sort_array);
 
-    info!("vdom for sort {:#?}", vdom);
+    // info!("vdom for sort {:#?}", vdom);
 
     // Run the virtual DOM forever and don't unmount it.
     vdom.forget();
